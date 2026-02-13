878283678964be5390d1652a92cfd44d1cc29a24
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2026-02-13 08:45:58 +0700

    setup

---
+++ src/components/auth/sign-up-form.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+const SignUpForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return <div></div>;
+};
+
+export default SignUpForm;
